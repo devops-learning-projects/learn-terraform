247baf375cd5f50c1b8ca8d2be174d6585b3e183
Mainul Islam <mainulislam6168@gmail.com>
2025-09-12 17:30:21 +0600

    HCL data types and variable types

---
+++ 02-variables/variable.tf
@@ -0,0 +1,61 @@
+# x1=abc in shell
+# x1: abc in ansible
+# and this is in terraform
+variable "x1" {
+  default = "abc"
+}
+
+output "x1" {
+  # variable var.x1 for the direct variable usage
+  value = var.x1
+}
+
+output "x2" {
+   # variable with combine with string then use ${var.x1}
+  value = "value of x1 is ${var.x1}"
+}
+
+## Data type in terraform(HCL)
+# 1. string--> use inside the double quotes
+# 2. Integer(number)--> use without quotes
+# 3. Boolean --> use without quote
+
+variable "string" {
+  default = "abc"
+}
+
+variable "number" {
+  default = 33.4
+}
+
+variable "Boolean" {
+  default = false
+}
+
+## Variables Types
+# Plain
+# List
+# Map/Dictionary
+
+variable "plain" {
+  default = "abc"
+}
+
+variable "list" {
+  default = ["xyz", 376, true]
+}
+
+output "list1" {
+  value = var.list[0]
+}
+
+variable "map" {
+  default = {
+    key  = "value"
+    name = "devOps"
+  }
+}
+
+output "map2" {
+  value = var.map["name"]
+}
