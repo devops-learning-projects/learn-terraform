c78c42f7f8fe45395dcc5cbc5ef89e57107a340f
Mainul Islam <mainulislam6168@gmail.com>
2025-09-12 11:35:58 +0600

    aws instance creation

--- 01-sample/sample.tf
+++ 01-sample/sample.tf
@@ -1,3 +1,10 @@
-provider "aws" {
+provider "aws" {}
 
+resource "aws_instance" "web" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "web-dev"
+  }
 }
