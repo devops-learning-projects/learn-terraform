1c802385939f9e4db0261d46a1ebf7fc0b781310
Mainul Islam <mainulislam6168@gmail.com>
2025-09-13 08:54:57 +0600

    for_each loop

---
+++ 04-loops2/sample.tf
@@ -0,0 +1,27 @@
+# to create the resource 10 time just set count value as 10
+resource "null_resource" "dummy" {
+  count = 10
+}
+
+provider "aws" {}
+variable "instances" {
+  default = {
+    frontend = {
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      instance_type = "t3.small"
+    }
+  }
+}
+resource "aws_instance" "web" {
+  for_each      = var.instances
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = each.value["instance_type"]
+  subnet_id     = "subnet-0a6dbf7c9698842d3"
+
+  tags = {
+    # Name = "web-dev-${count.index +1}"
+    Name = each.key
+  }
+}
