9ec9cdb3cab7602ccc1c70a8b50357bd6ae03304
Mainul Islam <mainulislam6168@gmail.com>
2025-09-13 08:32:47 +0600

    instance creation

--- 03-loops3/sample.tf
+++ 03-loops3/sample.tf
@@ -11,7 +11,6 @@ resource "aws_instance" "web" {
   count = length(var.instances)
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-  subnet_id     = "subnet-0a6dbf7c9698842d3"
 
   tags = {
     # Name = "web-dev-${count.index +1}"
