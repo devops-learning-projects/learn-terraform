0637daa42fb81d485ab8b6ae83ed22870bc700b3
Mainul Islam <mainulislam6168@gmail.com>
2025-09-13 08:39:34 +0600

    instance creation

--- 03-loops3/sample.tf
+++ 03-loops3/sample.tf
@@ -1,19 +1,42 @@
 # to create the resource 10 time just set count value as 10
-resource "null_resource" "dummy" {
-  count = 10
+# resource "null_resource" "dummy" {
+#   count = 10
+# }
+#
+# provider "aws" {}
+# variable "instances" {
+#   default = ["frontend", "catalogue"]
+# }
+# resource "aws_instance" "web" {
+#   count = length(var.instances)
+#   ami           = "ami-09c813fb71547fc4f"
+#   instance_type = "t3.micro"
+#   subnet_id     = "subnet-0a6dbf7c9698842d3"
+#
+#   tags = {
+#     # Name = "web-dev-${count.index +1}"
+#     Name = var.instances[count.index]
+#   }
+# }
+provider "aws" {}
+
+variable "ami" {
+  default = "ami-09c813fb71547fc4f"
 }
 
-provider "aws" {}
+variable "instance_type" {
+  default = "t3.small"
+}
+
 variable "instances" {
   default = ["frontend", "catalogue"]
 }
-resource "aws_instance" "web" {
-  count = length(var.instances)
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
 
+resource "aws_instance" "frontend" {
+  count         = length(var.instances)
+  ami           = var.ami
+  instance_type = var.instance_type
   tags = {
-    # Name = "web-dev-${count.index +1}"
     Name = var.instances[count.index]
   }
 }
