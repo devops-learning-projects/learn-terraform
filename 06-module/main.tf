435f1d92f66fbefc7b673b7175441413051cfbbd
Mainul Islam <mainulislam6168@gmail.com>
2025-09-13 17:14:37 +0600

    provisioner

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -2,7 +2,7 @@ module "sample" {
   source = "./sample-module"
   instance_type = var.instance_type
 }
-
+# module is used to keep code dry.
 # source can be multiple sources
 
 variable "instance_type" {
