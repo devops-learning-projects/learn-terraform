66ed9258fcf3775bf6c456550b9cab647f60825c
Mainul Islam <mainulislam6168@gmail.com>
2025-09-14 09:48:32 +0600

    hashicorp vault configuration

---
+++ 09-hashicorp-vault/test.tf
@@ -0,0 +1,24 @@
+data "vault_generic_secret" "sample" {
+  path = "kv/demo"
+}
+
+# Rundeck Provider, for example
+# For this example, in Vault there is a key named "auth_token" and the value is the token we need to keep secret.
+# In general usage, replace "auth_token" with the key you wish to extract from Vault.
+
+provider "vault" {
+  address        = "http://<ip>:8200"
+  token          = var.token
+}
+
+variable "token" {}
+
+output "secret" {
+  value = data.vault_generic_secret.sample
+  sensitive = true
+}
+
+resource "local_file" "foo" {
+  content = data.vault_generic_secret.sample.data["mypass"]
+  filename = "/tmp/secret"
+}
